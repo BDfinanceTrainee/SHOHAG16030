b69e80ec84e4ff9eeb0c396d6257cdf0af006193
Sohag16030 <shohag.mbstu.it16030@gmail.com>
2021-01-18 15:20:36 +0600

    day3 FullCrudeUsingDatatables(ajaxCall)

---
+++ DatatablesCrude/DatatablesCrude/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using DatatablesCrude.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatatablesCrude.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetEmployees()
+        {
+            using (MyDatabaseEntities dc =new MyDatabaseEntities())
+            {
+                var emoloyees = dc.tbl_Employee.OrderBy(a => a.FirstName).ToList();
+                return Json(new { data = emoloyees }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.tbl_Employee.Where(a => a.EmployeeID == id).FirstOrDefault();
+                return View(v);
+            }
+        }
+        [HttpPost]
+        public ActionResult Save(tbl_Employee emp)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                {
+                    if (emp.EmployeeID > 0)
+                    {
+                        //Edit 
+                        var v = dc.tbl_Employee.Where(a => a.EmployeeID == emp.EmployeeID).FirstOrDefault();
+                        if (v != null)
+                        {
+                            v.FirstName = emp.FirstName;
+                            v.LastName = emp.LastName;
+                            v.EmailID = emp.EmailID;
+                            v.City = emp.City;
+                            v.Country = emp.Country;
+                        }
+                    }
+                    else
+                    {
+                        //Save
+                        dc.tbl_Employee.Add(emp);
+                    }
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status} };
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.tbl_Employee.Where(a => a.EmployeeID == id).FirstOrDefault();
+                if (v != null)
+                {
+                    return View(v);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteEmployee(int id)
+        {
+            bool status = false;
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.tbl_Employee.Where(a => a.EmployeeID == id).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.tbl_Employee.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
---
+++ DatatablesCrude/DatatablesCrude/Models/Extented/tbl_Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatatablesCrude.Models
+{
+    [MetadataType(typeof(tbl_EmployeeMetadata))]
+    public partial class tbl_Employee
+    {
+    }
+    public class tbl_EmployeeMetadata
+    {
+        [Required(AllowEmptyStrings = false,ErrorMessage ="Please Provide First name")]
+        public string FirstName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide Last name")]
+        public string LastName { get; set; }
+
+
+    }
+}
