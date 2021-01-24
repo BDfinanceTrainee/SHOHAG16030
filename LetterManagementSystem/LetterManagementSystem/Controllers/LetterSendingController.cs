13fe162e818c172d87f6460d6e127b722a4f0725
Sohag16030 <shohag.mbstu.it16030@gmail.com>
2021-01-24 10:22:34 +0600

    ExceptHomePageAndIndexDesignIsCompleted

---
+++ LetterManagementSystem/LetterManagementSystem/Controllers/LetterSendingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LetterManagementSystem.Controllers
+{
+    public class LetterSendingController : Controller
+    {
+        // GET: LetterSending
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
